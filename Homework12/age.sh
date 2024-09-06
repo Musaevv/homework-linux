858963502f45a9dcc702ea5a025f477e8826c03f
EC2 Default User <ec2-user@ip-172-31-22-165.ec2.internal>
2024-09-06 23:32:51 +0000

    calculator

---
+++ Homework12/age.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+#
+#
+
+now=2024
+
+read -p "Enter your birth year " num1
+
+echo "Your age now is "$(($now - $num1))""
+
+echo "Your age in 2020 "$((2020 - $num1))""
+
+echo "Your age in 2030 "$((2030 - $num1))""
+
+
---
+++ Homework12/calculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+read -p "Enter first number " num1
+read -p "Enter second number " num2
+
+echo "Sum of this numbers is "$(($num1 + $num2))""
+
+echo "Difference of this numbers is "$(($num1 - $num2))""
+
+echo "Product of this number is "$(($num1 * $num2))""
+
+echo "Quotient of this number is "$(($num1 / $num2))""
+
+
+
+
