55781e4e157e40cc780402ce74f0f6adcbbffc7a
Ubuntu <ubuntu@ip-172-31-26-26.ec2.internal>
2024-09-06 21:48:35 +0000

    Using for loop in bash scripting

---
+++ table.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+num=5
+
+for i in {1..10};
+do
+	echo "$num multiple by $i = $((num * i))"
+done
+
+echo
+
+num=6
+
+for i in {1..10};
+do
+        echo "$num multiple by $i = $((num * i))"
+done
+
+
+echo
+
+num=7
+
+for i in {1..10};
+do
+        echo "$num multiple by $i = $((num * i))"
+done
+
+
+	
